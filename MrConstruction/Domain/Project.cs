9e74fb6311293c7cd682cf0101edacc74671a3ce
wyijx <jack.n.hutson@gmail.com>
2016-02-08 10:04:39 -0600

    added project model

---
+++ MrConstruction/Domain/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MrConstruction.Domain {
+    public class Project : IDbEntity, IActivatable {
+
+        public enum Status {
+            ToDo,
+            AwaitingEstimate,
+            InProgress,
+            PendingReview,
+            Completed
+        }
+
+        public int Id { get; set; }
+        public bool Active { get; set; }
+        public string Description { get; set; }
+        public Client Client { get; set; }
+        public IList<Upload> Uploads { get; set; }
+        public Status State { get; set; }
+        public IList<Job> JobList { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime EstStart { get; set; }
+        public DateTime EstCompleted { get; set; }
+    }
+}
