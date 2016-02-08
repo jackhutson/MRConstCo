d7a3fd5821c0798c2deb6972b1f05753c8bc8b08
Zachary Mixon <zg.mixoncode@gmail.com>
2016-02-08 10:15:25 -0600

    Commit for Location and Job Models

---
+++ MrConstruction/Domain/Job.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MrConstruction.Domain
+{
+    public class Job : IDbEntity, IActivatable
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Estimate { get; set; }
+
+        public string Contractor { get; set; }
+
+        public enum Status
+        {
+            ToDo,
+            AwaitingEstimate,
+            InProgress,
+            PendingReview,
+            Completed
+        }
+
+        public DateTime Deadline { get; set; }
+
+        public Location Location { get; set; }
+
+        public Project Project { get; set; }
+
+        public bool Active { get; set; }
+
+    }
+}
