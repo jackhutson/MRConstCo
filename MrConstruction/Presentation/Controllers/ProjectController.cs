0e22b996be0711330d1ee3e2442b89f66b3340da
wyijx <jack.n.hutson@gmail.com>
2016-02-09 14:54:05 -0600

    modified api routes, removed projectdetails controller

--- MrConstruction/Presentation/Controllers/ProjectController.cs
+++ MrConstruction/Presentation/Controllers/ProjectController.cs
@@ -15,8 +15,19 @@ namespace MrConstruction.Presentation.Controllers
         public ProjectController(ProjectService projectServ) {
             _projectServ = projectServ;
         }
+
+        [HttpGet]
         public IList<ProjectDTO> Get() {
             return _projectServ.ListProjects();
+        }
+
+        [HttpGet]
+        public ProjectDTO Get(int id) {
+            return _projectServ.GetOneProject(id);
+        }
+        [HttpPost]
+        public IHttpActionResult Post(NewProjectDTO newProject) {
+            return _projectServ.AddNewProject(newProject);
         }
     }
 }
