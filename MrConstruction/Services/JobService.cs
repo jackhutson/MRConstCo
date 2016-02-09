ac83b495fcd54de855f8698538d9af714017dee4
Sarah Kim <sarahywloo@gmail.com>
2016-02-09 10:48:48 -0600

    Updated domain models, edited job service layer

--- MrConstruction/Services/JobService.cs
+++ MrConstruction/Services/JobService.cs
@@ -1,4 +1,6 @@
-﻿using MrConstruction.Domain.Identity;
+﻿using Microsoft.AspNet.Identity;
+using MrConstruction.Domain;
+using MrConstruction.Domain.Identity;
 using MrConstruction.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -11,12 +13,17 @@ namespace MrConstruction.Services.Models
     {
         private JobRepository _jobRepo;
         private UserRepository _userRepo;
+        private ProjectRepository _projectRepo;
+        private ApplicationUserManager _appUserRepo;
 
-        public JobService(JobRepository jobRepo, UserRepository userRepo) {
+        public JobService(JobRepository jobRepo, UserRepository userRepo, ProjectRepository projectRepo, ApplicationUserManager appUserRepo) {
             _jobRepo = jobRepo;
             _userRepo = userRepo;
+            _projectRepo = projectRepo;
+            _appUserRepo = appUserRepo;
         }
 
+        //Get Job Details
         public JobDetailDTO GetJobDetails(int id, string username) {
 
             return (from j in _jobRepo.Get(id)
@@ -27,7 +34,9 @@ namespace MrConstruction.Services.Models
                         Description = j.Description,
                         Estimate = (u.Roles.FirstOrDefault(r => r.RoleId == Role.Admin)!= null) ? j.Estimate : (decimal?)null,
                         Name = j.Name,
-                        Project = j.Project,
+                        Project = new ProjectDTO() {
+                            Title = j.Project.Title
+                        },
                         State = j.State,
                         Contractor = new ContractorUserDTO() {
                             Name = j.Contractor.Name,
@@ -35,5 +44,40 @@ namespace MrConstruction.Services.Models
                         }
                     }).FirstOrDefault();                    
         } 
+
+        //Add a new job
+        public JobDetailDTO AddJob(JobDetailDTO dto, int id, string id2, string username) {
+
+            var contractor = _appUserRepo.FindById(id2);
+            var project = _projectRepo.Get(id).FirstOrDefault();
+
+            var job = new Job() {
+                Name = dto.Name,
+                Description = dto.Description,
+                Estimate = dto.Estimate,
+                Contractor = contractor,
+                State = dto.State,
+                Deadline = dto.Deadline,
+                Project = project
+            };
+
+            _jobRepo.Add(job);
+            _jobRepo.SaveChanges();
+
+            return new JobDetailDTO() {
+                Name = job.Name,
+                Description = job.Description,
+                Estimate = job.Estimate,
+                Contractor = new ContractorUserDTO() {
+                    Name = job.Contractor.Name,
+                    CompanyName = job.Contractor.CompanyName
+                },
+                State = job.State,
+                Deadline = job.Deadline,
+                Project = new ProjectDTO() {
+                    Id = job.Project.Id
+                }
+            };
+        }
     }
 }
