3e61b2b8ce508a8d82f5f4fa9cdf88ceee2539c4
wyijx <jack.n.hutson@gmail.com>
2016-02-10 11:51:56 -0600

    testing projectDetails controller showmodal

--- MrConstruction/Presentation/ngApp/controllers/newJobController.ts
+++ MrConstruction/Presentation/ngApp/controllers/newJobController.ts
@@ -8,13 +8,7 @@
         constructor(private $uibModalInstance, private $http: ng.IHttpService, private $location: ng.ILocationService, private $routeParams) { }
         
         public addTask(task): void {
-            this.$http.post(`/api/project/${task.project.id}/newTask`, task)
-                .then((response) => {
-                    this.$location.path('/projectDetails');
-                })
-                .catch((response) => {
-                    console.log(response);
-                });
+
         }
     }
 }
