9aa525b496a74baada53c4042107dbbf3e148147
wyijx <jack.n.hutson@gmail.com>
2016-02-10 13:27:41 -0600

    fixed newTask options logic

--- MrConstruction/Presentation/ngApp/controllers/newJobController.ts
+++ MrConstruction/Presentation/ngApp/controllers/newJobController.ts
@@ -5,7 +5,10 @@
         public task;
         public selectedContractor;
 
-        constructor(private $uibModalInstance, private $http: ng.IHttpService, private $location: ng.ILocationService, private $routeParams){ }
+        constructor(private $uibModalInstance, private $http: ng.IHttpService,
+            private $location: ng.ILocationService, private $routeParams) {
+            
+        }
         
         public addTask(task): void {
 
