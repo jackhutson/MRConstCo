cdce039ed3f75498d831542d36faa7447a075af3
Sarah Kim <sarahywloo@gmail.com>
2016-02-11 16:59:59 -0600

    created new project modal

--- MrConstruction/Presentation/ngApp/controllers/ProjectController.ts
+++ MrConstruction/Presentation/ngApp/controllers/ProjectController.ts
@@ -1,12 +1,46 @@
 ﻿namespace MrConstruction.Controllers {
     export class ProjectController {
+
         public projects;
-        constructor(private $http: ng.IHttpService) {
+        public modalInstance;
+
+        constructor(private $uibModal, private $http: ng.IHttpService) {
             $http.get('/api/project')
                 .then((response) => {
                     this.projects = response.data;
                 });
         }
-       
+
+        public showModal(): void {
+            this.modalInstance = this.$uibModal.open({
+                templateUrl: '/Presentation/ngApp/views/newProject.html',
+                controller: MrConstruction.Controllers.NewProjectController,
+                controllerAs: 'controller',
+                size: 'lg',
+                backdrop: true,
+            });
+
+            this.modalInstance.result
+                .then((newProject) => {
+                    this.$http.post('/api/project', newProject)
+                        .then((response) => {
+                            this.projects = response.data;
+                            console.log("post successful");
+                        })
+                })
+                .catch((dismiss) => {
+                });
+
+            //    public newProject(project) {
+            //    console.log(project);
+            //    this.$http.post('/api/project', project
+            //    ).then((response) => {
+            //        this.$location.path("#/projectDetails")
+            //        console.log("post successful");
+            //    }).catch((response) => {
+            //        console.log(`uh oh, error ${response}`);
+            //    });
+            //}
+        }
     }
 }
