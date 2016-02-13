23d3dc409f3370b04f9b147afd35e98283e17443
Sarah Kim <sarahywloo@gmail.com>
2016-02-13 12:29:15 -0600

    Changed upload domain model

--- MrConstruction/Domain/Upload.cs
+++ MrConstruction/Domain/Upload.cs
@@ -6,9 +6,16 @@ using System.Web;
 
 namespace MrConstruction.Domain {
     public class Upload: IDbEntity, IActivatable {
+
+        public enum Classification {
+            Picture,
+            Blueprint
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public Classification Type { get; set; }
 
         [InverseProperty("Uploads")]
         public Project Project { get; set; }
